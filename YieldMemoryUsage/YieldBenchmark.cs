4629bdde8d4831d7c733d8e643f13ac887a42211
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-11-27 21:16:49 +0700

    add yield bencmark

---
+++ YieldMemoryUsage/YieldBenchmark.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace YieldMemoryUsage;
+
+[MemoryDiagnoser(false)]
+public class YieldBenchmark
+{
+    [Params(10_000, 100_000,1_000_000)]
+    public int ItemsCount { get; set; }
+
+    [Benchmark]
+    public void ReturnItems()
+    {
+        foreach (var _ in ReturnWholeItems())
+        {
+        }
+    }
+
+    [Benchmark]
+    public void ReturnItems_Yield()
+    {
+        foreach (var _ in YieldReturnItems())
+        {
+        }
+    }
+
+    private IEnumerable<int> ReturnWholeItems()
+    {
+        var list = new List<int>(ItemsCount);
+        for (int i = 0; i < ItemsCount; i++)
+            list.Add(i);
+
+        return list;
+    }
+
+    private IEnumerable<int> YieldReturnItems()
+    {
+        for (int i = 0; i < ItemsCount; i++)
+            yield return i;
+    }
+}
